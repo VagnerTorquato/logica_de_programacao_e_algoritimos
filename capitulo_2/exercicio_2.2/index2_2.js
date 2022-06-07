50f0cda1bd602739359380d3081e3e9c87ee61e6
Vagner Torquato <vagnertorquato01@gmail.com>
2022-06-07 11:42:55 -0300

    Commit incial

---
+++ capitulo_2/exercicio_2.2/index2_2.js
@@ -0,0 +1,17 @@
+//Isto Cria uma referencia ao form e aos elementos h3 e h4 (resposta).
+const frm = document.querySelector('form');
+const resp1 = document.querySelector('h3');
+const resp2 = document.querySelector('h4');
+
+frm.addEventListener('submit', (e) => {
+    const titulo = frm.inTitulo.value;
+    const duracao = Number(frm.inDuracao.value);
+
+    const horas = Math.floor(duracao / 60);
+    const minutos = duracao % 60;
+
+    resp1.innerText = titulo;
+    resp2.innerText = `${horas}horas(s) ${minutos}minutos(s)`;
+    e.preventDefault();
+
+})
