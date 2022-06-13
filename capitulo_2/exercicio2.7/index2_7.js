47b2716543f787d50f9514210cb22d01d58da3e3
Vagner Torquato <vagnertorquato01@gmail.com>
2022-06-13 09:31:35 -0300

    Inclusão de Exercicios

--- capitulo_2/exercicio2.7/index2_7.js
+++ capitulo_2/exercicio2.7/index2_7.js
@@ -1,6 +1,6 @@
 const frm = document.querySelector("form");
-const resp1 = document.querySelector("#outResp1");
-const resp2 = document.querySelector("#outResp2");
+const resp1 = document.getElementById("outResp1");
+const resp2 = document.getElementById("outResp2");
 
 frm.addEventListener("submit", (e) => {
     const produto = frm.inProduto.value;
---
+++ capitulo_3/ex3_1.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+const numero1 = Number(prompt("1° número: "));
+const numero2 = Number(prompt("2° número: "));
+const soma = numero1 + numero2;
+console.log(`A soma dos números é: ${soma}`);
